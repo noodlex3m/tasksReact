0d9a23af2d5b113e2deb376c9bdce69a600bf746
Sergii <noodlex3m@gmail.com>
2025-07-27 11:14:08 +0300

    fix to do List

--- my-first-vite-app/src/App.jsx
+++ my-first-vite-app/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    "Вивчити компоненти",
-    "Створити To-Do List",
-  ]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      return JSON.parse(savedTasks);
+    } else {
+      return [];
+    }
+  });
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
   function addTask(taskName) {
     setTasks([...tasks, taskName]);
+  }
+  function deleteTask(taskToDelete) {
+    const newTask = tasks.filter((task) => task !== taskToDelete);
+    setTasks(newTask);
   }
   return (
     <div className="App">
       <h1>Мій React To-Do List</h1>
       <TaskForm onAddTask={addTask} />
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} />
     </div>
   );
 }
--- my-first-vite-app/src/components/TaskList.jsx
+++ my-first-vite-app/src/components/TaskList.jsx
@@ -2,7 +2,10 @@ function TaskList(props) {
   return (
     <ul>
       {props.tasks.map((task, index) => (
-        <li key={index}>{task}</li>
+        <li key={index}>
+          {task}
+          <button onClick={() => props.onDeleteTask(task)}>Видалити</button>
+        </li>
       ))}
     </ul>
   );
