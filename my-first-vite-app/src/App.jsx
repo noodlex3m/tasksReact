187270d2518c6940aae7eab40f58c985fbb892a6
Sergii <noodlex3m@gmail.com>
2025-08-04 23:21:17 +0300

    fix todo List

--- my-first-vite-app/src/App.jsx
+++ my-first-vite-app/src/App.jsx
@@ -4,31 +4,32 @@ import TaskList from "./components/TaskList";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState(() => {
-    const savedTasks = localStorage.getItem("tasks");
-    if (savedTasks) {
-      return JSON.parse(savedTasks);
-    } else {
-      return [];
-    }
-  });
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  });
-  function addTask(taskName) {
-    setTasks([...tasks, taskName]);
-  }
-  function deleteTask(taskToDelete) {
-    const newTask = tasks.filter((task) => task !== taskToDelete);
-    setTasks(newTask);
-  }
-  return (
-    <div className="App">
-      <h1>Мій React To-Do List</h1>
-      <TaskForm onAddTask={addTask} />
-      <TaskList tasks={tasks} onDeleteTask={deleteTask} />
-    </div>
-  );
+	const [tasks, setTasks] = useState(() => {
+		const savedTasks = localStorage.getItem("tasks");
+		return savedTasks ? JSON.parse(savedTasks) : [];
+	});
+	useEffect(() => {
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	});
+	function addTask(taskName) {
+		const newTask = {
+			id: Date.now(),
+			text: taskName,
+		};
+		setTasks([...tasks, newTask]);
+	}
+	function deleteTask(taskToDelete) {
+		const newTask = tasks.filter((task) => task.id !== taskToDelete);
+		setTasks(newTask);
+	}
+	return (
+		<div className="App">
+			<h1>Мій React To-Do List</h1>
+			<p>Залишилось завдань: {tasks.length}</p>
+			<TaskForm onAddTask={addTask} />
+			<TaskList tasks={tasks} onDeleteTask={deleteTask} />
+		</div>
+	);
 }
 
 export default App;
--- my-first-vite-app/src/components/TaskList.jsx
+++ my-first-vite-app/src/components/TaskList.jsx
@@ -1,14 +1,16 @@
 function TaskList(props) {
-  return (
-    <ul>
-      {props.tasks.map((task, index) => (
-        <li key={index}>
-          {task}
-          <button onClick={() => props.onDeleteTask(task)}>Видалити</button>
-        </li>
-      ))}
-    </ul>
-  );
+	return props.tasks.length === 0 ? (
+		<p>У вас немає завдань. Час відпочити!</p>
+	) : (
+		<ul>
+			{props.tasks.map((task) => (
+				<li key={task.id}>
+					<span>{task.text}</span>
+					<button onClick={() => props.onDeleteTask(task.id)}>Видалити</button>
+				</li>
+			))}
+		</ul>
+	);
 }
 
 export default TaskList;
